f8d9d3fdde29b72e9aec69da13ac8f17d826d027
Christian <christian.albrandaguirre@cesunbc.edu.mx>
2026-07-08 20:44:44 -0700

    forms

--- LemonRestaurant2/ReservationForm.swift
+++ LemonRestaurant2/ReservationForm.swift
@@ -8,11 +8,93 @@
 import SwiftUI
 
 struct ReservationForm: View {
+    // constants
+    let restaurantName = "Little Lemon"
+    let maxGuest = 10
+    let maxChildren = 10
+    // State variables ->
+    // if this value changes, update UI
+    
+    @State private var userName: String = ""
+    @State private var guestCount: Int = 1
+    @State private var phoneNumber: String = ""
+    @State private var preview : String = ""
+    @State private var occasionText: String = ""
+    @State private var childrenCount: Int = 1
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form{
+            //header
+            Section{
+                HStack{
+                    Image(systemName: "fork.knife")
+                        .foregroundStyle(.green)
+                        .font(.title)
+                    VStack(alignment: .leading){
+                        Text(restaurantName)
+                            .font(.title2)
+                            .bold()
+                        Text("Reservation form")
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                        
+                    }
+                    
+                    
+                }
+            }
+            //reservation details
+            Section(header:Text("Reservation Details")){
+                TextField("Name", text:$userName)
+                    .textInputAutocapitalization(.words)
+                    .autocorrectionDisabled(true)
+                // $ - > this view can READ and WRITE the value
+                
+                Stepper("Guests: \(guestCount)",value:$guestCount,in:1...maxGuest)
+            }
+            
+            //contact
+            Section(header:Text("Contact Information")){
+                TextField("Phone Number", text:$phoneNumber)
+                    .keyboardType(.numberPad)
+
+            }
+            
+            Section(header:Text("Optional")){
+                TextField("Occasion", text:$occasionText)
+                    .textInputAutocapitalization(.words)
+                    .autocorrectionDisabled(true)
+                Stepper("Children: \(childrenCount)",value:$childrenCount, in:1...maxChildren)
+            }
+            
+            //actions
+            Section(header:Text("Actions")){
+                Button("Preview reservation details"){
+                    preview =
+                        """
+                        Name: \(userName)
+                        Guest: \(guestCount)
+                        Phone: \(phoneNumber)
+                        Children: \(childrenCount)
+                        Ocassion: \(occasionText)
+                        
+                        """
+                }
+            }
+            //preview
+            Section(header:Text("Preview")){
+                Text(preview)
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+                    .padding(4)
+                    .textSelection(.enabled)
+            }
+        }
+        .navigationTitle("Reservation")
     }
 }
 
 #Preview {
-    ReservationForm()
+    NavigationStack {
+        ReservationForm()
+    }
 }
--- Practice/Variables.playground/Contents.swift
+++ Practice/Variables.playground/Contents.swift
@@ -1,3 +1,62 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// variables var
+// constants let
+
+var restaurantName = "Little lemon"
+print("Welcome to \(restaurantName)")
+
+restaurantName = "Little lemon bistro"
+print("Now we are calles \(restaurantName)")
+
+// constants cannot change
+let city = "New York"
+//city = "Chicago" // Error: Cannot assign to value
+print("Our restaurant is located in \(city)")
+
+//Strings
+var specialDish = "Pasta primavera"
+
+//Integers
+var aviableTables = 5
+
+//Doubles
+var dishesPrice = 12.50
+
+//Boolean
+var isOpen = true
+
+print("Today's special: \(specialDish) - $\(dishesPrice)")
+
+//Cahllenge #1
+var numberOfTables = 10
+let ownerName = "Christian"
+
+print("Little lemon has \(numberOfTables) tables and is owned by \(ownerName)")
+
+//Challenge #2
+var wave = "Hello!"
+var numOfPlayer = 4
+var coins = 233.12
+var continueGame = true
+print("\(wave) player \(numOfPlayer) you have \(coins) coins! do you wish to continue the game? \(continueGame)")
+
+//data types
+// we can also explicity define the types
+
+var anotherDish: String = "Pizza"
+var tableCount: Int = 10
+var price: Double = 8.99
+var openTable: Bool = true
+
+// use math operator
+let pastaPrice: Double = 10.50
+let saladPrice: Double = 6.25
+let total = pastaPrice + saladPrice
+print("The total is \(total)")
+
+//Add a 15% tip to the total and print the final amount
+let tip = total * 0.15
+print ("The tip is \(tip)")
+let totalWithTip = total + tip
+print ("The total with tip is \(totalWithTip)")
