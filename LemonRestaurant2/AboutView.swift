d6a2ccbf73f6d20768a9ebfcebab7308877a91e7
Christian <christian.albrandaguirre@cesunbc.edu.mx>
2026-07-08 20:41:50 -0700

    Assignment2 forms

---
+++ LemonRestaurant2/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  LemonRestaurant2
+//
+//  Created by Christian-SDGKU on 08/07/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ LemonRestaurant2/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  LemonRestaurant2
+//
+//  Created by Christian-SDGKU on 08/07/26.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
