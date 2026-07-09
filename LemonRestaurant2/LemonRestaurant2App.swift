ea8cc449f54bfae182c405664bc940417042edb1
Christian <christian.albrandaguirre@cesunbc.edu.mx>
2026-07-08 17:41:15 -0700

    Initial Commit

---
+++ LemonRestaurant2/LemonRestaurant2App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurant2App.swift
+//  LemonRestaurant2
+//
+//  Created by Christian-SDGKU on 08/07/26.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurant2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
