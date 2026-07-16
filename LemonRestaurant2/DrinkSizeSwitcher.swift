4737bdcaf6caa9083762b1edf7cf680cf1fd160a
Christian <christian.albrandaguirre@cesunbc.edu.mx>
2026-07-15 21:19:34 -0700

    Drink size switcher

--- LemonRestaurant2/DrinkSizeSwitcher.swift
+++ LemonRestaurant2/DrinkSizeSwitcher.swift
@@ -15,46 +15,52 @@ struct DrinkSizeSwitcher: View {
     // 3 = large
     
     var body: some View {
-        VStack {
-            ZStack {
-                switch size{
-                case 1:
-                    Text("🥤")
-                        .font(.system(size: 100))
-                case 2:
-                    Text("🥤")
-                        .font(.system(size: 140))
-                case 3:
-                    Text("🥤")
-                        .font(.system(size: 170))
-                default:
-                    Text("Error")
-                }
-            }.frame(height: 190).animation(.easeInOut(duration: 0.2), value: size)
-            
-            switch size{
-            case 1:
-                Text("Small Drink").padding(10).font(.system(size: 30))
-            case 2:
-                Text("Medium").padding(10).font(.system(size: 30))
-            case 3:
-                Text("Large").padding(10).font(.system(size: 30))
-            default :
-                Text("Error")
-            }
-            
-            HStack {
-                Button("Small"){
-                    size = 1
-                }
-                Button("Medium"){
-                    size = 2
-                }
-                Button("Large"){
-                    size = 3
-                }
+        ZStack{
+            Color.blue.ignoresSafeArea().opacity(0.50)
+            VStack {
+                ZStack {
+                    switch size{
+                    case 1:
+                        Text("🥤")
+                            .font(.system(size: 100))
+                    case 2:
+                        Text("🥤")
+                            .font(.system(size: 140))
+                    case 3:
+                        Text("🥤")
+                            .font(.system(size: 170))
+                    default:
+                        Text("Error")
+                    }
+                }.frame(height: 190).animation(.easeInOut(duration: 0.2), value: size)
+                
+                HStack{
+                    switch size{
+                    case 1:
+                        Text("Small Drink")
+                    case 2:
+                        Text("Medium Drink")
+                    case 3:
+                        Text("Large Drink")
+                    default :
+                        Text("Error")
+                    }
+                }.padding().font(.system(size: 30)).animation(.easeInOut(duration: 0.2), value: size)
+                
+                HStack {
+                    Button("Small"){
+                        size = 1
+                    }
+                    Button("Medium"){
+                        size = 2
+                    }
+                    Button("Large"){
+                        size = 3
+                    }
+                }.buttonStyle(.borderedProminent).tint(.blue).padding().font(.system(size: 20))
             }
         }
+        
         
 /*
         Text("\(counter)")
